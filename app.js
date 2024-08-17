b51b51587dff27ec5b704efd130f124181a81900
ljhobson <louishobson5@gmail.com>
2024-08-17 20:27:50 +1200

    added stuff

--- app.js
+++ app.js
@@ -1,10 +1,28 @@
 var http = require("http");
+var fs = require('fs');
 var PORT = process.env.PORT || 8000;
 
 
-http.createServer((req, res) => {
-	console.log("Server running on port " + PORT);
-
-	res.write("Hello Guitar");
-	res.end();
+http.createServer((request, response) => {
+	var path = request.url;
+	console.log(path);
+	
+	if (path === "/style.css") {
+		fs.readFile("style.css", function(error, content) {
+			response.writeHead(200, { "Content-Type": "style/css" });
+			response.end(content); //, "utf-8");
+		});
+	} else if (path === "/favicon.ico") {
+		fs.readFile("favicon.ico", function(error, content) {
+			response.writeHead(200, { "Content-Type": "image/x-icon" });
+			response.end(content); //, "utf-8");
+		});
+	} else {
+		fs.readFile("index.html", function(error, content) {
+			response.writeHead(200, { "Content-Type": "text/html" });
+			response.end(content); //, "utf-8");
+		});
+	}
 }).listen(PORT);
+
+console.log("Server running on port " + PORT);
