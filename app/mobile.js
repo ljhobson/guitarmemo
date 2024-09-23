798a0232a68c43a76f4a97df6dd28a285d826a05
ljhobson <louishobson5@gmail.com>
2024-09-24 00:28:16 +1200

    surely that isn't the issue

--- app/mobile.js
+++ app/mobile.js
@@ -1,11 +1,11 @@
 alert("this is right at the start of the page");
-
-if (!navigator.serviceWorker.controller) {
-	navigator.serviceWorker.register("/app/service-worker.js").then(function(registration) {
-		console.log("Service worker has been registered for scope: " + registration.scope);
-	});
-}
-
+//
+//if (!navigator.serviceWorker.controller) {
+//	navigator.serviceWorker.register("/app/service-worker.js").then(function(registration) {
+//		console.log("Service worker has been registered for scope: " + registration.scope);
+//	});
+//}
+//
 alert("this is after the service worker stuff");
 
 
