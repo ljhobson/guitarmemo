3ae43df0d121557d575c7cadd05f61b7a56ef4f3
ljhobson <louishobson5@gmail.com>
2024-09-24 00:13:27 +1200

    lakfsdh

--- app/mobile.js
+++ app/mobile.js
@@ -1,4 +1,4 @@
-?/*
+/*
 if (!navigator.serviceWorker.controller) {
 	navigator.serviceWorker.register("/app/service-worker.js").then(function(registration) {
 		console.log("Service worker has been registered for scope: " + registration.scope);
